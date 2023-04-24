3034ec64e5bcf133852867d6e591de38938e0c73
Jacksoan Eufrosino <jacksoan.elmar@hotmail.com>
2023-04-24 15:59:20 -0300

    Consertado o problema de logica do script 1018

--- 1018.py
+++ 1018.py
@@ -1,49 +1,40 @@
-valor = int(input())
+entrada = int(input())
+print(entrada)
 
-resto = 0
-novo_valor = valor
+n100 = 0
+n50 = 0
+n20 = 0
+n10 = 0
+n5 = 0
+n2 = 0
+n1 = 0
 
-nota_100 = 0
-nota_50 = 0
-nota_20 = 0
-nota_10 = 0
-nota_5 = 0
-nota_2 = 0
-nota_1 = 0
+n100 = entrada // 100
+entrada = entrada % 100
 
-if valor >= 100:
-    resto = valor % 100
-    nota_100 = valor // 100
+n50 = entrada // 50
+entrada = entrada % 50
 
-if resto >= 50:
-    nota_50 = resto // 50
-    resto = resto % 50
+n20 = entrada // 20
+entrada = entrada % 20
 
-if resto >= 20:
-    nota_20 = resto // 20
-    resto = resto % 20
+n10 = entrada // 10
+entrada = entrada % 10
 
-if resto >= 10:
-    nota_10 = resto // 10
-    resto = resto % 10
+n5 = entrada // 5
+entrada = entrada % 5
 
-if resto >= 5:
-    nota_5 = resto // 5
-    resto = resto % 5
+n2 = entrada // 2
+entrada = entrada % 2
 
-if resto >= 2:
-    nota_2 = resto // 2
-    resto = resto % 2
+n1 = entrada // 1
+entrada = entrada % 1
 
-if resto >= 1:
-    nota_1 = resto // 1
-    resto = resto % 1
 
-print(valor)
-print(nota_100, 'nota(s) de R$ 100,00')
-print(nota_50, 'nota(s) de R$ 50,00')
-print(nota_20, 'nota(s) de R$ 20,00')
-print(nota_10, 'nota(s) de R$ 10,00')
-print(nota_5, 'nota(s) de R$ 5,00')
-print(nota_2, 'nota(s) de R$ 2,00')
-print(nota_1, 'nota(s) de R$ 1,00')
+print(n100, 'nota(s) de R$ 100,00')
+print(n50, 'nota(s) de R$ 50,00')
+print(n20, 'nota(s) de R$ 20,00')
+print(n10, 'nota(s) de R$ 10,00')
+print(n5, 'nota(s) de R$ 5,00')
+print(n2, 'nota(s) de R$ 2,00')
+print(n1, 'nota(s) de R$ 1,00')
