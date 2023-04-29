1fba6536e33ec1e3fb7fe0be22232152337ac83c
Jacksoan Eufrosino <jacksoan.elmar@hotmail.com>
2023-04-29 10:32:55 -0300

    Adicionado 10 novos scripts (+ .tmux.conf)

--- 1001.py
+++ 1001.py
@@ -3,11 +3,6 @@ num = int(input('Diga me um numero: '))
 for i in range(0, num):
     print('Meu nome nao é jonny')
 
-
-def fun(suahsuahsuh):
-    print('oxe')
-
-
 if 1 != 0:
     print('')
     for i in range(0, 14):
---
+++ 1041.py
@@ -0,0 +1,21 @@
+x, y = input().split()
+x, y = float(x), float(y)
+
+if x > 0 and y > 0:
+    print('Q1')
+elif x < 0 and y > 0:
+    print('Q2')
+elif x < 0 and y < 0:
+    print('Q3')
+elif x > 0 and y < 0:
+    print('Q4')
+
+if x > 0 or x < 0:
+    if y == 0:
+        print('Eixo X')
+elif y > 0 or y < 0:
+    if x == 0:
+        print('Eixo Y')
+elif x == 0 and y == 0:
+    print('Origem')
+
